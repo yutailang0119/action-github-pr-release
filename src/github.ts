e17a25091189ebd760c0e141bd634a1af249aad8
yutailang0119 <yutailang0119@gmail.com>
2021-05-20 01:01:33 +0900

    Fix github to class

--- src/github.ts
+++ src/github.ts
@@ -1,26 +1,43 @@
 import * as github from '@actions/github'
-import {Repo} from './Repo'
 import {Repository, Commit, PullRequest, Maybe} from '@octokit/graphql-schema'
 
-async function associatedPullRequest(
-  token: string,
-  repo: Repo,
-  expression: string
-): Promise<Maybe<PullRequest>> {
-  const octokit = github.getOctokit(token)
+export class GitHub {
+  token: string
+  owner: string
+  repo: string
+  productionBranch: string
+  stagingBranch: string
 
-  const query = `
-  query associatedPullRequest($owner: String!, $repo: String!, $expression: String!) {
-    repository(owner: $owner, name: $repo) {
-      object(expression: $expression) {
-        ... on Commit {
-          associatedPullRequests(first: 1, orderBy: {field: UPDATED_AT, direction: ASC}) {
-            edges {
-              node {
-                title
-                number
-                author {
-                  login
+  constructor(
+    token: string,
+    owner: string,
+    repo: string,
+    productionBranch: string,
+    stagingBranch: string
+  ) {
+    this.token = token
+    this.owner = owner
+    this.repo = repo
+    this.productionBranch = productionBranch
+    this.stagingBranch = stagingBranch
+  }
+
+  async associatedPullRequest(expression: string): Promise<Maybe<PullRequest>> {
+    const octokit = github.getOctokit(this.token)
+
+    const query = `
+    query associatedPullRequest($owner: String!, $repo: String!, $expression: String!) {
+      repository(owner: $owner, name: $repo) {
+        object(expression: $expression) {
+          ... on Commit {
+            associatedPullRequests(first: 1, orderBy: {field: UPDATED_AT, direction: ASC}) {
+              edges {
+                node {
+                  title
+                  number
+                  author {
+                    login
+                  }
                 }
               }
             }
@@ -28,42 +45,45 @@ async function associatedPullRequest(
         }
       }
     }
+    `
+    const {repository} = await octokit.graphql<{repository: Repository}>(
+      query,
+      {
+        owner: this.owner,
+        repo: this.repo,
+        expression: expression
+      }
+    )
+    const commit = repository.object as Commit
+    if (commit.associatedPullRequests?.edges === null) return null
+    if (commit.associatedPullRequests?.edges === undefined) return null
+    if (commit.associatedPullRequests?.edges[0]?.node === undefined) return null
+    const pullRequest = commit.associatedPullRequests?.edges[0]?.node
+    return new Promise(resolve => {
+      resolve(pullRequest)
+    })
   }
-  `
-  const {repository} = await octokit.graphql<{repository: Repository}>(query, {
-    owner: repo.owner,
-    repo: repo.owner,
-    expression: expression
-  })
-  const commit = repository.object as Commit
-  if (commit.associatedPullRequests?.edges === null) return null
-  if (commit.associatedPullRequests?.edges === undefined) return null
-  if (commit.associatedPullRequests?.edges[0]?.node === undefined) return null
-  const pullRequest = commit.associatedPullRequests?.edges[0]?.node
-  return new Promise(resolve => {
-    resolve(pullRequest)
-  })
-}
 
-async function compareSHAs(token: string, repo: Repo): Promise<string[]> {
-  const octokit = github.getOctokit(token)
+  async compareSHAs(): Promise<string[]> {
+    const octokit = github.getOctokit(this.token)
 
-  // https://docs.github.com/en/rest/reference/repos#compare-two-commits
-  const shas = await octokit
-    .paginate(octokit.rest.repos.compareCommits, {
-      owner: repo.owner,
-      repo: repo.name,
-      base: repo.productionBranch,
-      head: repo.stagingBranch,
-      per_page: 100
-    })
-    .then(result => {
-      return result.commits.map(commit => {
-        return commit.sha
+    // https://docs.github.com/en/rest/reference/repos#compare-two-commits
+    const shas = await octokit
+      .paginate(octokit.rest.repos.compareCommits, {
+        owner: this.owner,
+        repo: this.repo,
+        base: this.productionBranch,
+        head: this.stagingBranch,
+        per_page: 100
       })
-    })
+      .then(result => {
+        return result.commits.map(commit => {
+          return commit.sha
+        })
+      })
 
-  return new Promise(resolve => {
-    resolve(shas)
-  })
+    return new Promise(resolve => {
+      resolve(shas)
+    })
+  }
 }
