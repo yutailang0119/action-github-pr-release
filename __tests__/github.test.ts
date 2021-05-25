5ce44e86cecc9803aa931d34cdf8f396fdbfa7cf
yutailang0119 <yutailang0119@gmail.com>
2021-05-26 02:02:31 +0900

    Add test to github.compareSHAs

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,2 +1,15 @@
 import * as process from 'process'
 import {GitHub} from '../src/github'
+
+test('compareSHAs', async () => {
+  const token = process.env.TEST_TOKEN ?? ''
+  const owner = process.env.TEST_REPOSITORY_OWNER ?? ''
+  const repo = process.env.TEST_REPOSITORY_NAME ?? ''
+  const productionBranch = process.env.TEST_PRODUCTION_BRANCH ?? ''
+  const stagingBranch = process.env.TEST_STAGING_BRANCH ?? ''
+
+  const github = new GitHub(token, owner, repo, productionBranch, stagingBranch)
+
+  const result = await github.compareSHAs()
+  expect(result).not.toEqual([])
+})
