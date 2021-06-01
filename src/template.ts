aed2b83061775104463a32bfb29c70ccdcdddbf7
yutailang0119 <yutailang0119@gmail.com>
2021-06-02 01:55:09 +0900

    Add template to create check list

---
+++ src/template.ts
@@ -0,0 +1,7 @@
+import {PullRequestItem} from '../src/github'
+
+export function checkList(pullRequests: PullRequestItem[]): string {
+  return pullRequests.reduce((v: string, pr: PullRequestItem): string => {
+    return v + `- [ ] #${pr.number} ${pr.title} @${pr.author}\n`
+  }, '')
+}
