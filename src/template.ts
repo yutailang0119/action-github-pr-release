6ce517122cbc4e7e1e9ccbf07b5644d5a272b410
yutailang0119 <yutailang0119@gmail.com>
2021-06-03 01:02:07 +0900

    Fix: Unexpected string concatenation

--- src/template.ts
+++ src/template.ts
@@ -2,6 +2,6 @@ import {PullRequestItem} from '../src/github'
 
 export function checkList(pullRequests: PullRequestItem[]): string {
   return pullRequests.reduce((v: string, pr: PullRequestItem): string => {
-    return v + `- [ ] #${pr.number} ${pr.title} @${pr.author}\n`
+    return `${v}- [ ] #${pr.number} ${pr.title} @${pr.author}\n`
   }, '')
 }
