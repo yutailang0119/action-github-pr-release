9160e9d56d67577e0be238992209e5999a473b27
yutailang0119 <yutailang0119@gmail.com>
2021-06-04 01:40:22 +0900

    Fix test for template

--- __tests__/template.test.ts
+++ __tests__/template.test.ts
@@ -1,11 +1,14 @@
 import {PullRequestItem} from '../src/github'
-import {checkList} from '../src/template'
+import {Template} from '../src/template'
+
+const pullRequests: PullRequestItem[] = [
+  {number: 123, title: 'AAA', author: 'yutailang0119'},
+  {number: 456, title: 'BBB', author: 'y7g'}
+]
 
 test('checkList', () => {
-  const pullRequests: PullRequestItem[] = [
-    {number: 123, title: 'AAA', author: 'yutailang0119'},
-    {number: 456, title: 'BBB', author: 'y7g'}
-  ]
-  const result = checkList(pullRequests)
-  expect(result).toEqual(`- #123 AAA @yutailang0119\n- #456 BBB @y7g\n`)
+  const template = new Template(pullRequests)
+  expect(template.checkList()).toEqual(
+    `- #123 AAA @yutailang0119\n- #456 BBB @y7g\n`
+  )
 })
