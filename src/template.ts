39565d22c5db9a3c7bdf45d093c781de1e4e1448
yutailang0119 <yutailang0119@gmail.com>
2021-06-04 01:39:45 +0900

    Fix template to class

--- src/template.ts
+++ src/template.ts
@@ -1,7 +1,18 @@
 import {PullRequestItem} from '../src/github'
 
-export function checkList(pullRequests: PullRequestItem[]): string {
-  return pullRequests.reduce((v: string, pr: PullRequestItem): string => {
-    return `${v}- #${pr.number} ${pr.title} @${pr.author}\n`
-  }, '')
+export class Template {
+  pullRequests: PullRequestItem[]
+
+  constructor(pullRequests: PullRequestItem[]) {
+    this.pullRequests = pullRequests
+  }
+
+  checkList(): string {
+    return this.pullRequests.reduce(
+      (v: string, pr: PullRequestItem): string => {
+        return `${v}- #${pr.number} ${pr.title} @${pr.author}\n`
+      },
+      ''
+    )
+  }
 }
