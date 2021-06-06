e80e931292bbba9623ec9072b28dceda85dde42a
yutailang0119 <yutailang0119@gmail.com>
2021-06-06 19:09:10 +0900

    Fix main to implement

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,52 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {GitHub} from './github'
+import {Template} from './template'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('token')
+    const owner = core.getInput('owner')
+    const repo = core.getInput('repo')
+    const productionBranch = core.getInput('production_branch')
+    const stagingBranch = core.getInput('staging_branch')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const github = new GitHub(token, owner, repo)
 
-    core.setOutput('time', new Date().toTimeString())
+    const compareSHAs = await github.compareSHAs(
+      productionBranch,
+      stagingBranch
+    )
+    const pullRequests = await Promise.all(
+      compareSHAs.map(async sha => {
+        return github.associatedPullRequest(sha)
+      })
+    )
+
+    const template = new Template(
+      new Date(),
+      pullRequests.flatMap(pr => pr ?? [])
+    )
+
+    const existingPullRequest = await github.detectExistingPullRequest(
+      productionBranch,
+      stagingBranch
+    )
+
+    if (existingPullRequest.pullRequest === null) {
+      await github.createPullRequest(
+        existingPullRequest.repositoryId,
+        productionBranch,
+        stagingBranch,
+        template.title(),
+        template.checkList()
+      )
+    } else {
+      await github.updatePullRequest(
+        existingPullRequest.pullRequest.id,
+        template.title(),
+        template.checkList()
+      )
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
