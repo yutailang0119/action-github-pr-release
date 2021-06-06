49e3428a241360d5b96faf53b4e7fdedf485c1d1
yutailang0119 <yutailang0119@gmail.com>
2021-06-07 01:32:08 +0900

    Fix test for main

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,14 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_TOKEN'] = process.env.TEST_TOKEN
+  process.env['INPUT_OWNER'] = process.env.TEST_REPOSITORY_OWNER
+  process.env['INPUT_NAME'] = process.env.TEST_REPOSITORY_NAME
+  process.env['INPUT_PRODUCTION_BRANCH'] = process.env.TEST_PRODUCTION_BRANCH
+  process.env['INPUT_STAGING_BRANCH'] = process.env.TEST_STAGING_BRANCH
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
