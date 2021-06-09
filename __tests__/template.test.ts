50ef70f0561329bcb5ecd82996494c6a9754fea7
yutailang0119 <yutailang0119@gmail.com>
2021-06-09 22:16:43 +0900

    Fix template.test to cleanup

--- __tests__/template.test.ts
+++ __tests__/template.test.ts
@@ -1,19 +1,18 @@
 import {Template} from '../src/template'
 
-const date = new Date()
-
-const pullRequests = [
-  {number: 123, title: 'AAA', author: 'yutailang0119'},
-  {number: 456, title: 'BBB', author: 'y7g'},
-  {number: 123, title: 'AAA', author: 'yutailang0119'},
-]
-
 test('title', () => {
-  const template = new Template(date, pullRequests)
+  const date = new Date()
+  const template = new Template(date, [])
   expect(template.title()).toEqual(`Release ${date}`)
 })
 
 test('checkList', () => {
+  const date = new Date()
+  const pullRequests = [
+    {number: 123, title: 'AAA', author: 'yutailang0119'},
+    {number: 456, title: 'BBB', author: 'y7g'},
+    {number: 123, title: 'AAA', author: 'yutailang0119'}
+  ]
   const template = new Template(date, pullRequests)
   expect(template.checkList()).toEqual(
     `- #123 AAA @yutailang0119\n- #456 BBB @y7g\n`
