10b6a5548e42bf1a6e12c8fec21bcc02ad96e847
yutailang0119 <yutailang0119@gmail.com>
2021-06-10 21:37:01 +0900

    Fix github.test for env variables

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -2,13 +2,13 @@ import * as process from 'process'
 import {GitHub} from '../src/github'
 
 test('detectExistingPullRequest', async () => {
-  const token = process.env.TEST_TOKEN ?? ''
-  const owner = process.env.TEST_REPOSITORY_OWNER ?? ''
-  const name = process.env.TEST_REPOSITORY_NAME ?? ''
-  const productionBranch = process.env.TEST_PRODUCTION_BRANCH ?? ''
-  const stagingBranch = process.env.TEST_STAGING_BRANCH ?? ''
-  const repositoryId = process.env.TEST_REPOSITORY_ID ?? ''
-  const pullRequestId = process.env.TEST_PULL_REQUEST_ID ?? ''
+  const token = process.env.TEST_TOKEN
+  const owner = process.env.TEST_REPOSITORY_OWNER
+  const name = process.env.TEST_REPOSITORY_NAME
+  const productionBranch = process.env.TEST_PRODUCTION_BRANCH
+  const stagingBranch = process.env.TEST_STAGING_BRANCH
+  const repositoryId = process.env.TEST_REPOSITORY_ID
+  const pullRequestId = process.env.TEST_PULL_REQUEST_ID
 
   const github = new GitHub(token, owner, name)
 
@@ -22,11 +22,11 @@ test('detectExistingPullRequest', async () => {
 })
 
 test('associatedPullRequest', async () => {
-  const token = process.env.TEST_TOKEN ?? ''
-  const owner = process.env.TEST_REPOSITORY_OWNER ?? ''
-  const name = process.env.TEST_REPOSITORY_NAME ?? ''
-  const commitSHA = process.env.TEST_COMMIT_SHA ?? ''
-  const pullRequestNumber = process.env.TEST_PULL_REQUEST_NUMBER ?? ''
+  const token = process.env.TEST_TOKEN
+  const owner = process.env.TEST_REPOSITORY_OWNER
+  const name = process.env.TEST_REPOSITORY_NAME
+  const commitSHA = process.env.TEST_COMMIT_SHA
+  const pullRequestNumber = process.env.TEST_PULL_REQUEST_NUMBER
 
   const github = new GitHub(token, owner, name)
 
@@ -35,11 +35,11 @@ test('associatedPullRequest', async () => {
 })
 
 test('compareSHAs', async () => {
-  const token = process.env.TEST_TOKEN ?? ''
-  const owner = process.env.TEST_REPOSITORY_OWNER ?? ''
-  const name = process.env.TEST_REPOSITORY_NAME ?? ''
-  const productionBranch = process.env.TEST_PRODUCTION_BRANCH ?? ''
-  const stagingBranch = process.env.TEST_STAGING_BRANCH ?? ''
+  const token = process.env.TEST_TOKEN
+  const owner = process.env.TEST_REPOSITORY_OWNER
+  const name = process.env.TEST_REPOSITORY_NAME
+  const productionBranch = process.env.TEST_PRODUCTION_BRANCH
+  const stagingBranch = process.env.TEST_STAGING_BRANCH
 
   const github = new GitHub(token, owner, name)
 
