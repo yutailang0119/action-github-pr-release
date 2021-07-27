2350bf89dda64da6de4637a357ea1378e0a708b0
yutailang0119 <yutailang0119@gmail.com>
2021-07-27 22:39:59 +0900

    Fix inputs in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {GitHub} from './github'
 import {Template} from './template'
 
 async function run(): Promise<void> {
   try {
+    const repository = core.getInput('repository') ?? github.context.repo.repo
+    const splited = repository.split('/')
+    const owner = splited[0]
+    const name = splited[1]
+
     const token = core.getInput('token', {required: true})
-    const owner = core.getInput('owner', {required: true})
-    const name = core.getInput('name', {required: true})
     const productionBranch = core.getInput('production_branch')
     const stagingBranch = core.getInput('staging_branch')
     const isDryRun = core.getBooleanInput('dry_run')
