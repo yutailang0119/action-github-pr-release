139f0d4164928a65359af6daabda76edf50a14c5
yutailang0119 <yutailang0119@gmail.com>
2021-08-09 23:53:43 +0900

    Fix main.test to remove INPUT_REPOSITORY env

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,7 +5,6 @@ import * as path from 'path'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_TOKEN'] = process.env.TEST_TOKEN ?? ''
-  process.env['INPUT_REPOSITORY'] = process.env.TEST_REPOSITORY ?? ''
   process.env['INPUT_PRODUCTION_BRANCH'] =
     process.env.TEST_PRODUCTION_BRANCH ?? ''
   process.env['INPUT_STAGING_BRANCH'] = process.env.TEST_STAGING_BRANCH ?? ''
