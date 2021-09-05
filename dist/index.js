ee8ae98cb4bbc3d04f18566356c14d6418f44c2b
yutailang0119 <yutailang0119@gmail.com>
2021-09-06 02:38:27 +0900

    npm run package

--- dist/index.js
+++ dist/index.js
@@ -369,7 +369,7 @@ query ($owner: String!, $name: String!, $expression: String!) {
   repository(owner: $owner, name: $name) {
     object(expression: $expression) {
       ... on Commit {
-        associatedPullRequests(first: 1, orderBy: {field: UPDATED_AT, direction: ASC}) {
+        associatedPullRequests(first: 1, orderBy: {field: CREATED_AT, direction: ASC}) {
           edges {
             node {
               ... on PullRequest {
