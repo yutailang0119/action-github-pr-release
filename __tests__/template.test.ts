ef9a1cc73b56b0fd34cf6bc1f03a66496e1fd580
yutailang0119 <yutailang0119@gmail.com>
2021-09-12 16:19:27 +0900

    Update template.test to check sort

--- __tests__/template.test.ts
+++ __tests__/template.test.ts
@@ -9,9 +9,12 @@ test('body', () => {
   const date = new Date()
   const pullRequests = [
     {number: 123, author: 'yutailang0119'},
+    {number: 789, author: 'yutailang0119'},
     {number: 456, author: 'y7g'},
     {number: 123, author: 'yutailang0119'}
   ]
   const template = new Template(pullRequests)
-  expect(template.body).toEqual(`- #123 @yutailang0119\n- #456 @y7g\n`)
+  expect(template.body).toEqual(
+    `- #123 @yutailang0119\n- #456 @y7g\n- #789 @yutailang0119\n`
+  )
 })
